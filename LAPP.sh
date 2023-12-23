68cb16b72edba33f3034e6312a585baba74b9c00
Phillip Kirk <phillipjkirk1@gmail.com>
2023-12-23 13:36:30 +0000

    added LAPP.sh & fix LAMP.sh

--- LAPP.sh
+++ LAPP.sh
@@ -2,8 +2,8 @@ apt update && apt full-upgrade -y
 apt install nano git unzip wget sudo curl -y
 
 sudo apt-get install apache2 -y
-sudo apt install mariadb-server -y
-sudo apt-get install php php-mysql -y
+sudo apt install postgresql-14 -y
+sudo apt-get install php php-pgsql -y
 sudo service apache2 restart
 sudo apt-get install certbot -y
 sudo mkdir /var/phpmyadmin
@@ -16,9 +16,11 @@ sudo mv phpMyAdmin-*-all-languages phpmyadmin
 sudo mv phpmyadmin/config.sample.inc.php phpmyadmin/config.inc.php
 sudo mv phpmyadmin/* .
 sudo rm -rf phpmyadmin
-sudo service mariadb restart
-mysql_secure_installation
+sudo service postgresql restart
+
 
 openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 \
   -nodes -keyout server.key -out server.crt -subj "/CN=localhost" \
   -addext "subjectAltName=DNS:localhost,DNS:*.localhost,IP:127.0.0.1"
+
+sudo -i -u postgres
